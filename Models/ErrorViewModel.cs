7c0d5deacb775259424c60d317de43f4fa9bc616
Nilson Alfredo Fuentes Sierra <nilson.educacion@outlook.com>
2022-01-26 07:26:29 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppPruebaGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
